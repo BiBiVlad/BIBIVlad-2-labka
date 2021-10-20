dcc82ec3d0fd3fa3617cf4ce361fad9c691b6395
BiBiVlad <91695055+BiBiVlad@users.noreply.github.com>
2021-10-20 22:18:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from math import pi
 x = int(input())
-h = 0
 list = list()
-for i in str(pi):
-    h += 1
-    list.append(i)
-    if h == x+2:
-        break
-
-h = ''.join(list)
-print(h)
+def p1(x):
+    h= 0
+    for i in str(pi):
+        h += 1
+        list.append(i)
+        if h == x+2:
+            break
+    h = ''.join(list)
+    return h
+print(p1(x))
